cf201716b00ff7c26b8fc009b510bcf036dcba8e
A Naranjo <anara004@gmail.com>
2018-05-27 10:25:14 -0500

    continued to work on topic modeling

--- corpus.R
+++ corpus.R
@@ -20,13 +20,13 @@ dfCorpus2 <- tm_map(dfCorpus2, content_transformer(tolower))
 dfCorpus2 <- tm_map(dfCorpus2, removeWords, stopwords("english"))
 dfCorpus2 <- tm_map(dfCorpus2, stemDocument)
 
-myStopwords <- c('can', 'say', 'one', 'way', 'use', 'also', 'howev', 'tell', 'will', 'much', 'take'
+myStopwords <- c('can', 'say', 'one', 'way', 'use', 'also', 'howev', 'tell', 'will', 'much'
                  ,'tend', 'even', 'like', 'particular', 'rather', 'said', 'get', 'well', 'make', 'ask', 'come'
-                 ,'end', 'first', 'two', 'help', 'often', 'may', 'might', 'see', 'someth', 'thing', 'point'
-                 ,'post', 'look', 'right', 'now', 'think', 'anoth', 'put', 'set', 'new', 'want', 'sure', 'kind'
-                 ,'larg', 'yes', 'day', 'etc', 'quit', 'sinc', 'attempt', 'lack', 'seen', 'awar', 'littl', 'ever'
-                 ,'moreov', 'though', 'found', 'abl', 'enough', 'far', 'earli', 'away', 'achiev', 'draw', 'last'
-                 ,'brief', 'bit', 'entir', 'lot', 'wish', 'what', 'just', 'that', 'let', 'dont', 'cant', 'still')
+                 ,'end', 'first', 'two', 'often', 'may', 'might', 'see', 'someth', 'thing', 'point'
+                 ,'post', 'think', 'anoth', 'put', 'set', 'want', 'sure', 'kind'
+                 ,'yes', 'etc',  'sinc', 'attempt', 'lack', 'seen', 'awar', 'littl', 'ever'
+                 , 'what', 'just', 'that', 'let', 'dont', 'cant', 'still'
+                 , 'ill', 'ive', 'yall')
 
 dfCorpus2 <- tm_map(dfCorpus2, removeWords, myStopwords)
 
@@ -44,7 +44,7 @@ dtm <- DocumentTermMatrix(dfCorpus2)
 # findAssocs(dtm, "sex", 0.49)
 
 # Removes sparse terms
-dtm2 <- removeSparseTerms(dtm, 0.98)
+dtm2 <- removeSparseTerms(dtm, 0.989)
 inspect(dtm2)
 
 # Removes documents with no words
@@ -80,7 +80,7 @@ FindTopicsNumber_plot(topic_search)
 # lda_out <- LDA(dtm, k, method = 'Gibbs'
 #                , control = list(nstart = nstart, seed = seed, burnin = burnin, iter = iter, thin = thin))
 
-lda_out <- LDA(dtm3, 40)
+lda_out <- LDA(dtm3, 35)
 
 # save(lda_out, file = 'data/lda_100.object')
 # lda_out <- load('data/lda_10.object')
@@ -116,6 +116,9 @@ if (length(topics) == length(fcb_data2$text)) {
 }
 
 
+# filter(fcb_data2, label == 'Sex' & topic == 79)
+write(fcb_data2, '~/Desktop/fcb_data2.csv')
+
 # write_csv(arrange(fcb_data, topic), '~/Desktop/fcb_data_withTopics.csv')
 
 # View the probabilities of each topic
@@ -124,15 +127,17 @@ as.tibble(lda_out@gamma)
 # Create word probabilities for each document
 lda_word_probs <- tidy(lda_out, matrix = 'beta')
 
+
 # Displays word probabilities
 lda_top_terms <- lda_word_probs %>%
   group_by(topic) %>%
-  top_n(20, beta) %>%
+  top_n(10, beta) %>%
   ungroup() %>%
   arrange(topic, -beta)
 
 lda_top_terms %>%
   mutate(term = reorder(term, beta)) %>%
+  filter(topic %in% c(79, 92, 35, 72)) %>% 
   ggplot(aes(term, beta, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
